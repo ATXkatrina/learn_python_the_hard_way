656bd4f18fd6adc7d8f9a16039f9198cdf81f135
Katrina Tollin <ktollin@gmail.com>
2017-02-22 11:45:22 -0600

    numbers

---
+++ ex3.py
@@ -0,0 +1,12 @@
+print "counting chickens"
+
+print "Hens", 25 + 30 / 6
+
+print "roosers", 100 - 25 * 3 % 4
+
+print "now eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+print "is it greater?", 5 > -2
+print "is it greater or equal?", 5 >= -2
