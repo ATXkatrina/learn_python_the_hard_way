eaff0876331513353707e174cf80c86727246bbb
Katrina Tollin <ktollin@gmail.com>
2017-02-22 11:58:29 -0600

    more variables

---
+++ ex5.py
@@ -0,0 +1,13 @@
+my_name = 'Katrina G. Tollin'
+my_age = 29
+my_height = 64 #inches
+my_eyes = 'Green'
+my_teeth = 'white'
+my_hair = 'red'
+
+print "Let's talk about %s." % my_name
+print "She's %d inches tall." % my_height
+print "She has %s eyes and %s hair." % (my_eyes, my_hair)
+print "Her teeth are usually %s depending on the coffee." % my_teeth
+
+print "If I add %d and %d I get %d." % (my_age, my_height, my_age + my_height)
